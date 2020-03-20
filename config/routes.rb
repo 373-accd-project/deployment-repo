d89cad1b54a22e02dbeb05b73b32613e053a3c54
Shane Dasbach <sedasbac@andrew.cmu.edu>
2020-03-19 21:25:21 -0400

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :home
   root 'welcome#index'
+
+  get 'home' => 'home#index', as: :home
+
 end
