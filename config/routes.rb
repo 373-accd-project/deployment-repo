c1297165606de36d863d173eb899195889445697
Shane Dasbach <sedasbac@andrew.cmu.edu>
2020-03-19 21:35:05 -0400

    working locally

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :home
+  # resources :home
   root 'welcome#index'
 
   get 'home' => 'home#index', as: :home
-
+  post "home", to: "home#index"
 end
